c53826ff028aed371e6b810db85a341fd9ddbaa8
Renan Fonteles <renanfonteles@live.com>
2022-07-28 12:14:05 -0300

    [Refactor] Organize code into .py files for reuse in multiple notebooks.

--- base.py
+++ base.py
@@ -16,22 +16,23 @@ davisbouldin(k_list, k_centers):
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 
+
 def delta(ck, cl):
     values = np.ones([len(ck), len(cl)])*10000
-    
+
     for i in range(0, len(ck)):
         for j in range(0, len(cl)):
             values[i, j] = np.linalg.norm(ck[i]-cl[j])
-            
+
     return np.min(values)
-    
+
 def big_delta(ci):
     values = np.zeros([len(ci), len(ci)])
-    
+
     for i in range(0, len(ci)):
         for j in range(0, len(ci)):
             values[i, j] = np.linalg.norm(ci[i]-ci[j])
-            
+
     return np.max(values)
 
 
@@ -47,11 +48,11 @@ def dunn(k_list):
     deltas = np.ones([len(k_list), len(k_list)])*1000000
     big_deltas = np.zeros([len(k_list), 1])
     l_range = list(range(0, len(k_list)))
-    
+
     for k in l_range:
         for l in (l_range[0:k]+l_range[k+1:]):
             deltas[k, l] = delta(k_list[k], k_list[l])
-        
+
         big_deltas[k] = big_delta(k_list[k])
 
     di = np.min(deltas)/np.max(big_deltas)
@@ -68,7 +69,7 @@ def delta_fast(ck, cl, distances):
 def big_delta_fast(ci, distances):
     values = distances[np.where(ci)][:, np.where(ci)]
     #values = values[np.nonzero(values)]
-            
+
     return np.max(values)
 
 
@@ -84,29 +85,29 @@ def dunn_fast(points, labels):
     """
     distances = euclidean_distances(points)
     ks = np.sort(np.unique(labels))
-    
+
     deltas = np.ones([len(ks), len(ks)])*1000000
     big_deltas = np.zeros([len(ks), 1])
-    
+
     l_range = list(range(0, len(ks)))
-    
+
     for k in l_range:
         for l in (l_range[0:k]+l_range[k+1:]):
             deltas[k, l] = delta_fast((labels == ks[k]), (labels == ks[l]), distances)
-        
+
         big_deltas[k] = big_delta_fast((labels == ks[k]), distances)
 
     di = np.min(deltas)/np.max(big_deltas)
     return di
-    
-    
+
+
 def  big_s(x, center):
     len_x = len(x)
     total = 0
-        
+
     for i in range(len_x):
-        total += np.linalg.norm(x[i]-center)    
-    
+        total += np.linalg.norm(x[i]-center)
+
     return total/len_x
 
 
@@ -124,7 +125,7 @@ def davisbouldin(k_list, k_centers):
     len_k_list = len(k_list)
     big_ss = np.zeros([len_k_list], dtype=np.float64)
     d_eucs = np.zeros([len_k_list, len_k_list], dtype=np.float64)
-    db = 0    
+    db = 0
 
     for k in range(len_k_list):
         big_ss[k] = big_s(k_list[k], k_centers[k])
@@ -143,3 +144,77 @@ def davisbouldin(k_list, k_centers):
         db += np.max(values)
     res = db/len_k_list
     return res
+
+
+# --------------------------------------- Implemented by Rômulo ------------------------------------------------------ #
+from numpy.linalg import norm
+
+
+def DB(model, X, q=2, t=2):
+    k = len(model.cluster_centers_)  # number of clusters
+    db = 0
+    for i in range(k):
+        db += R(i, q, t, model, X)
+    db /= k
+    return db
+
+
+def R(i, q, t, model, X):
+    js = [j for j in range(len(model.cluster_centers_)) if j != i]  # for j!=i
+    R_iqt = 0  # R_iqt is always >= 0
+    for j in js:
+        temp = (S(i, q, model, X) + S(j, q, model, X)) / d(i, j, t, model)
+        R_iqt = temp if temp > R_iqt else R_iqt  # searching for the maximum
+    return R_iqt
+
+
+def S(i, q, model, X):
+    Vi = X[np.where(model.labels_ == i)]  # partition 'i'
+    wi = model.cluster_centers_[i]  # cluster center of partition 'i'
+    S_iq = 0
+    for x in Vi:
+        S_iq += norm(x - wi) ** q
+
+    S_iq = (S_iq / len(Vi)) ** (1 / q)
+    return S_iq
+
+
+def d(i, j, t, model):
+    wi = model.cluster_centers_[i]
+    wj = model.cluster_centers_[j]
+    d_ijt = 0
+    for m in range(len(wi)):
+        d_ijt += (abs(wi[m] - wj[m])) ** t
+    d_ijt = d_ijt ** (1 / t)
+    return d_ijt
+
+
+# %%
+
+from numpy import trace
+
+
+def CH(kmeans, points):
+    # ks is the vector of clusters. Ex: [0 1 2 3 4]
+    # N is the vector frequency of each label. Ex: [14 10 5 12]
+    ks, N = np.unique(np.sort(kmeans.labels_), return_counts=True)
+    x_bar = np.mean(points, axis=0).reshape(-1, 1)
+
+    # matriz dispersão entregrupos
+    Bk = np.zeros((points.shape[1], points.shape[1]))
+    # matriz dispersão intragrupo
+    Wk = np.zeros((points.shape[1], points.shape[1]))
+    for i in ks:  # para cada protótipo
+        wi = kmeans.cluster_centers_[i].reshape(-1, 1)  # protótipo do cluster 'i'
+        temp = wi - x_bar
+        Bk += N[i] * np.matmul(temp, temp.T)
+
+        Vi = points[kmeans.labels_ == i].T
+        for l in range(Vi.shape[1]):  # para cada elemento da partição 'i'
+            temp = Vi[:, l] - wi
+            Wk += np.matmul(temp, temp.T)
+
+    N = len(points)  # number of points
+    K = len(kmeans.cluster_centers_)  # number os clusters
+    ch = (trace(Bk) / (K - 1)) / (trace(Wk) / (N - K))
+    return ch
---
+++ local_learning.py
@@ -0,0 +1,75 @@
+import datetime
+import numpy as np
+
+from copy import copy
+
+
+class BiasModel:
+    """Class the implements a dummy model in case of homogenous region"""
+
+    def __init__(self, class_label):
+        self.class_label = class_label
+
+    def predict(self, X):
+        return np.vstack([self.class_label] * len(X))
+
+
+class LocalModel:
+    """Class of Local Models."""
+
+    def __init__(self, ClusterAlg, ModelAlg):
+        self.ClusterAlg = ClusterAlg
+        self.ModelAlg = ModelAlg
+
+        self.clusters = None
+        self.models = None
+        self.targets_dim_ = None
+
+        self.empty_regions = []
+
+    def fit(self, X, Y, verboses=0):
+        self.targets_dim_ = Y.shape[1]  # dimension of target values
+
+        # Clustering
+        if verboses == 1: print("Start of clusterization: {}".format(datetime.datetime.now()))
+
+        self.ClusterAlg.fit(X)
+        self.clusters = self.ClusterAlg.cluster_centers_
+
+        # Local models training
+        if verboses == 1: print("Start of local models training: {}".format(datetime.datetime.now()))
+
+        n_clusters = self.ClusterAlg.n_clusters
+        labels = self.ClusterAlg.labels_  # labels of each datapoints
+        self.models = [{}] * n_clusters
+        for i in range(n_clusters):  # for each region
+            Xi = X[np.where(labels == i)[0]]
+            Yi = Y[np.where(labels == i)[0]]
+
+            model_i = None
+            local_classes = np.unique(Yi, axis=0)  # list of local classes
+            if len(Xi) == 0:  # empty region
+                self.empty_regions.append(i)
+
+            elif len(local_classes) == 1:  # homogenous region, only one class
+                model_i = BiasModel(local_classes)
+
+            else:  # region not empty or homogeneous
+                self.ModelAlg.fit(Xi, Yi)
+                model_i = copy(self.ModelAlg)
+
+            self.models[i] = model_i
+
+    def predict(self, X, rounded=False):
+        predictions = np.zeros((
+            len(X),  # number of samples
+            self.targets_dim_  # size of output Y
+        ))
+
+        region_list = self.ClusterAlg.predict(X)  # .astype(int) # list of regions of each sample
+        for i in range(len(X)):
+            predictions[i, :] = self.models[region_list[i]].predict(X[i].reshape(1, -1))
+            # should I worry about regions with no models? (empty regions in training)
+            # isn't that a sign of bad clustering?
+
+        return predictions if not rounded else np.round(np.clip(predictions, 0, 1))
